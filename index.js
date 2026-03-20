31d7f6e8656bab55deda58ecd6452a229f6438a9
wolfyblue078 <wolfyblue078@gmail.com>
2026-03-20 19:35:19 +0530

    feat: create database connection 💭

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 import e from "express";
+import mongoose from "mongoose";
 
 const app = e();
+
+let mongoUrl = "mongodb+srv://admin:123@main.mkocfh6.mongodb.net/?appName=Main"
+let connection = async ()=>{
+    try{
+        await mongoose.connect(mongoUrl);
+        console.log("DataBase connected successfuly ✅")
+    }catch(err){
+        console.log("DB Connection error! -"+ err.message);
+    }
+}
+await connection();
 
 app.listen(3000, ()=>{
     console.log("app is running on port 3000!");
