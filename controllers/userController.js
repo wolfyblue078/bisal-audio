7819f23b3157827aee9c66908eb05215742577ef
wolfyblue078 <wolfyblue078@gmail.com>
2026-03-21 05:38:44 +0530

    feat: add user registration check and bcrypt for password hashing

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,19 @@
 import User from "../models/user.js"
+import bcrypt from "bcrypt"
 
 export const RegisterUser = async (req,res)=>{
     try {
         let data = req.body;
+
+        //check already registered
+        let user = await User.findOne({email: data.email});
+        if(user){
+            return res.status(400).json({
+                message: "user already registered 🧨"
+            })
+        }
+
+        data.password = await bcrypt.hash(data.password, 10);
         let newUser = new User(data);
 
         await newUser.save();
